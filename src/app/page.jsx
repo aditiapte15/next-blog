2703f0bdcb63d728310d19c70e28a8ee58da9b7e
Aditi Apte <aditiapte15@gmail.com>
2024-11-18 15:54:50 +0530

    Create Header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
-export default function Home() 
-{
+export default function Home() {
   return (
-    <div className='text-red-500'>Home</div>
+    <div className='bg-white text-black'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+          <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        </div>
+      </div>
+    </div>
   )
 }
