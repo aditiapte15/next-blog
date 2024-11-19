c1f43707ae4225f2579eb3c71f217214d3df7c94
Aditi Apte <aditiapte15@gmail.com>
2024-11-19 11:13:22 +0530

    Add next-themes to apply dark mode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,7 +7,6 @@ import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-
 export default function Header() {
   const path = usePathname();
   const { theme, setTheme } = useTheme();
@@ -30,57 +29,59 @@ export default function Header() {
       setSearchTerm(searchTermFromUrl);
     }
   }, [searchParams]);
-
   return (
-    <Navbar className="border-b-2">
+    <Navbar className='border-b-2'>
       <Link
-        href="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        href='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          News
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          News&apos;s
         </span>
         Blog
       </Link>
       <form onSubmit={handleSubmit}>
         <TextInput
-          type="text"
-          placeholder="Search..."
+          type='text'
+          placeholder='Search...'
           rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
+          className='hidden lg:inline'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex gap-2 md:order-2">
+      <div className='flex gap-2 md:order-2'>
         <Button
-          className="w-12 h-10 hidden sm:inline"
-          color="gray"
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
           pill
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         >
           {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        {/* Removed Clerk User Button Section */}
-        <Link href="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
+        
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Link href="/">
+        <Link href='/'>
           <Navbar.Link active={path === '/'} as={'div'}>
             Home
           </Navbar.Link>
         </Link>
-        <Link href="/about">
+        <Link href='/about'>
           <Navbar.Link active={path === '/about'} as={'div'}>
             About
+          </Navbar.Link>
+        </Link>
+        <Link href='/projects'>
+          <Navbar.Link active={path === '/projects'} as={'div'}>
+            Projects
           </Navbar.Link>
         </Link>
       </Navbar.Collapse>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
 export default function Home() {
   return (
-    <div className='bg-white text-black'>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-          <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        </div>
-      </div>
-    </div>
+    
+    <div> <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1></div>
+    
+         
   )
 }
