3093efa6c4cf654ab6cdb12c7d74d53e7eaa1275
Aditi Apte <aditiapte15@gmail.com>
2024-11-19 19:12:45 +0530

    Fixed Issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local';
 import './globals.css';
-import Header from './components/Header';
+import Header from '.src/components/Header';
 import { ThemeProvider } from 'next-themes';
 import ThemeCom from './components/ThemeCom';
 import { ClerkProvider } from '@clerk/nextjs';
